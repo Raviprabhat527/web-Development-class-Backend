1ccfeee01b1ed2af5f00a92db7c981997168610b
Raviprabhat527 <ravikumar312004@gmail.com>
2025-04-11 14:48:07 +0530

    class 2 React

---
+++ class 106 React/first-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Copyright &copy;  www.raviprabhat.com | All Right reserved
+    </div>
+  )
+}
+
+export default Footer
